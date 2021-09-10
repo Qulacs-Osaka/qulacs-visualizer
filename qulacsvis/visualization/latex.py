408959710d059a8c2e46d3dd9317163e6a892585
Hiroya-W <hyuyukun@gmail.com>
2021-09-10 21:37:10 +0900

    Add LatexCompiler class

---
+++ qulacsvis/visualization/latex.py
@@ -0,0 +1,57 @@
+import subprocess
+import tempfile
+
+
+class LatexCompiler:
+    """
+    Compile latex code to pdf.
+    """
+
+    def __init__(self) -> None:
+        """
+        Initialize the latex compiler.
+        """
+        if not self.has_pdflatex():
+            raise Exception("pdflatex not found.")
+
+    def compile(self, code: str, filename: str) -> None:
+        """
+        Compile the latex code.
+
+        Parameters
+        ----------
+        code : str
+            The latex code to compile.
+        filename : str
+            The filename of the latex code.
+        """
+        with tempfile.TemporaryDirectory() as tmpdir:
+            with open(tmpdir + "/" + filename + ".tex", "w") as f:
+                f.write(code)
+            try:
+                subprocess.run(
+                    [
+                        "pdflatex",
+                        "-halt-on-error",
+                        f"-output-directory={tmpdir}",
+                        tmpdir + "/" + filename + ".tex",
+                    ],
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.DEVNULL,
+                )
+            except subprocess.CalledProcessError:
+                raise Exception("pdflatex failed.")
+
+    def has_pdflatex(self) -> bool:
+        """
+        Check if latex is installed.
+        """
+        try:
+            subprocess.run(
+                ["pdflatex", "--version"],
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+            )
+            return True
+        except FileNotFoundError:
+            return False
