8e4aefdd371745061c8c54faa3189d80c6870450
kawamochi <kotaro111024@gmail.com>
2021-09-28 15:43:37 +0900

    modify circuit drawer

--- qulacsvis/visualization/circuit_drawer.py
+++ qulacsvis/visualization/circuit_drawer.py
@@ -5,8 +5,11 @@ from typing import Optional, Union
 from PIL import Image
 from qulacsvis.utils.latex import _LatexCompiler, _PDFtoImage
 
+from .latex import _generate_latex_source
+
 
 def circuit_drawer(
+    circuit,
     output_method: Optional[str] = None,
     *,
     ppi: int = 150,
@@ -31,7 +34,7 @@ def circuit_drawer(
 
     elif output_method == "latex":
         with tempfile.TemporaryDirectory() as tmpdir:
-            latex_source = _generate_latex_source()
+            latex_source = _generate_latex_source(circuit)
             latex = _LatexCompiler()
             pdftoimage = _PDFtoImage()
 
@@ -42,42 +45,9 @@ def circuit_drawer(
             return image
 
     elif output_method == "latex_source":
-        return _generate_latex_source()
+        return _generate_latex_source(circuit)
 
     else:
         raise ValueError(
             "Invalid output_method. Valid options are: 'text', 'latex', 'latex_source'."
         )
-
-
-def _generate_latex_source() -> str:
-    """
-    Generates a latex source code of a circuit.
-
-    Returns
-    -------
-    str
-        The latex source code of the circuit.
-    """
-
-    generated_latex_code = r"""
-\documentclass[12pt,border={25pt 5pt 5pt 5pt}]{standalone}
-
-\usepackage[T1]{fontenc}
-\usepackage{lmodern}
-\usepackage[cmex10]{amsmath}
-
-\usepackage{qcircuit}
-\usepackage{braket}
-
-\begin{document}
-
-\Qcircuit @C=1em @R=.7em {
-  \lstick{\ket{0}} & \gate{H} & \ctrl{1} & \qw          & \qw      & \meter \\
-  \lstick{\ket{0}} & \qw      & \targ    & \ctrl{1}     & \qw      & \meter \\
-  \lstick{\ket{0}} & \qw      & \gate{H} & \control \qw & \gate{H} & \meter
-}
-
-\end{document}
-    """
-    return generated_latex_code
