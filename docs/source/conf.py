3f7685b822dbe17a8fbc483d4d303b47c03f7b21
Hiroya-W <hyuyukun@gmail.com>
2021-12-07 16:12:10 +0900

    Update documentation page

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,6 +42,7 @@ mathjax_path = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
+master_doc = "contents"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
---
+++ docs/source/contents.rst
@@ -0,0 +1,19 @@
+===========================
+Documentation for qulacsvis
+===========================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   index
+   modules
+   qulacsvis.visualization.circuit_drawer
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
