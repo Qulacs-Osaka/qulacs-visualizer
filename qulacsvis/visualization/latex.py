04e46dad77d4539bca0041c1abfd5fb8f0884fb0
kawamochi <kotaro111024@gmail.com>
2021-12-17 12:45:26 +0900

    modify latex.py to use circuit parser

--- qulacsvis/visualization/latex.py
+++ qulacsvis/visualization/latex.py
@@ -1,4 +1,5 @@
 from qulacs import QuantumCircuit
+from qulacsvis.visualization.circuit_parser import CircuitParser
 
 
 def _generate_latex_source(circuit: QuantumCircuit) -> str:
@@ -39,8 +40,11 @@ def _generate_latex_source(circuit: QuantumCircuit) -> str:
         "ParametricRY": r"{pRY}",
         "ParametricRZ": r"{pRZ}",
         "ParametricPauliRotation": r"{pPR}",
+        "wire": r"¥qw",
+        "ghost": "ghost",
     }
-    qubit_count = circuit.get_qubit_count()
+    parser = CircuitParser(circuit)
+    qubit_count = parser.qubit_count
 
     gate_latex = [
         [
@@ -49,27 +53,14 @@ def _generate_latex_source(circuit: QuantumCircuit) -> str:
         ]
         for i in range(qubit_count)
     ]
-    gate_latex_part = [r"\qw" for _ in range(qubit_count)]
 
-    gate_num = circuit.get_gate_count()
-    for i in range(gate_num):
-        gate = circuit.get_gate(i)
-        if len(gate.get_target_index_list()) == 0:
-            print(
-                """CAUTION: The {}-th Gate you added is skipped.\
-                This gate does not have "target_qubit_list".""".format(
-                    i
-                )
-            )
-            continue
-        target_index_list = gate.get_target_index_list()
-        control_index_list = gate.get_control_index_list()
-        name_latex_part = gate_dict[gate.get_name()]
-
-        if len(control_index_list) > 0:
-            for qubit in range(qubit_count):
-                gate_latex[qubit].append(gate_latex_part[qubit])
-                gate_latex_part[qubit] = r"\qw"
+    for i in range(len(parser.gate_info[0])):
+        gate_latex_part = [r"\qw" for _ in range(qubit_count)]
+        for j in range(qubit_count):
+            gate_data = parser.gate_info[j][i]
+            name_latex_part = gate_dict[gate_data['raw_text']]
+            target_index_list = gate_data['target_bit']
+            control_index_list = gate_data['control_bit']
 
             for target_index in target_index_list:
                 name_latex = ""
@@ -86,46 +77,14 @@ def _generate_latex_source(circuit: QuantumCircuit) -> str:
                         name_latex = r"\ghost" + name_latex_part
                 gate_latex_part[target_index] = name_latex
 
-            target_index = target_index_list[0]
             for control_index in control_index_list:
                 gate_latex_part[control_index] = (
-                    r"\ctrl{" + str(target_index - control_index) + r"}"
+                    r"\ctrl{" + str(target_index_list[0] - control_index) + r"}"
                 )
 
-            for qubit in range(qubit_count):
-                gate_latex[qubit].append(gate_latex_part[qubit])
-                gate_latex_part[qubit] = r"\qw"
-
-            continue
-
-        conflict = False
-        for target_index in target_index_list:
-            if gate_latex_part[target_index] != r"\qw":
-                conflict = True
-
-        if conflict:
-            for qubit in range(qubit_count):
-                gate_latex[qubit].append(gate_latex_part[qubit])
-                gate_latex_part[qubit] = r"\qw"
-
-        for target_index in target_index_list:
-            name_latex = ""
-            if len(target_index_list) == 1:
-                if name_latex_part == r"\targ":
-                    name_latex = name_latex_part
-                else:
-                    name_latex = r"\gate" + name_latex_part
-            else:
-                if target_index == target_index_list[0]:
-                    name_latex = r"\multigate{" + str(len(target_index_list) - 1) + r"}"
-                    name_latex += name_latex_part
-                else:
-                    name_latex = r"\ghost" + name_latex_part
-            gate_latex_part[target_index] = name_latex
-
-    for qubit in range(qubit_count):
-        gate_latex[qubit].append(gate_latex_part[qubit])
-        gate_latex[qubit].append(r"\qw")
+        
+        for qubit in range(qubit_count):
+            gate_latex[qubit].append(gate_latex_part[qubit])
 
     circuit_latex_part = [" & ".join(gate_latex[i]) for i in range(qubit_count)]
     circuit_latex = (r" \\" + "\n").join(circuit_latex_part)
