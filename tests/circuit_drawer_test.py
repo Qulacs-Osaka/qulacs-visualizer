9d193938dbee34a4ecaa718d82babeb6557ef6fc
Hiroya-W <hyuyukun@gmail.com>
2021-12-21 17:57:53 +0900

    Modified to table-driven testing

--- tests/circuit_drawer_test.py
+++ tests/circuit_drawer_test.py
@@ -1,52 +1,46 @@
+import os
+
 import pytest
-from qulacs import QuantumCircuit
 
 from qulacsvis import circuit_drawer
+from qulacs import QuantumCircuit
 
+from .circuit_test_data import load_circuit_data, empty_circuit
+from typing import Any
 
-def test_output_method_None() -> None:
-    circuit = QuantumCircuit(2)
-    circuit.add_X_gate(0)
-    circuit.add_Y_gate(1)
-    circuit.add_Z_gate(2)
-    circuit.add_dense_matrix_gate(
-        [0, 1], [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]]
+EXPECTED_STR_PATH = "tests/baseline/text_circuit_drawer/"
+
+circuit_data = load_circuit_data()
+# empty_circuit will throw an IndexError Exception
+circuit_data.pop("empty_circuit")
+
+test_table = []
+
+for key, circuit in circuit_data.items():
+    test_table.append(
+        (
+            pytest.param(
+                circuit,
+                os.path.join(EXPECTED_STR_PATH, key + ".txt"),
+                id=key,
+            )
+        )
     )
-    circuit.add_CNOT_gate(2, 0)
-    circuit.add_X_gate(2)
-
-    circuit_drawer(circuit, output_method=None)
-    # TODO: check if text is generated
 
 
-def test_output_method_text() -> None:
-    circuit = QuantumCircuit(2)
-    circuit.add_X_gate(0)
-    circuit.add_Y_gate(1)
-    circuit.add_Z_gate(2)
-    circuit.add_dense_matrix_gate(
-        [0, 1], [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]]
-    )
-    circuit.add_CNOT_gate(2, 0)
-    circuit.add_X_gate(2)
+def test_empty_circuit() -> None:
+    circuit = empty_circuit()
+    with pytest.raises(IndexError):
+        circuit_drawer(circuit, output_method="text")
 
+
+@pytest.mark.parametrize("circuit,expected_path", test_table)
+def test_text_circuit_drawer(
+    circuit: QuantumCircuit, expected_path: str, capfd: Any
+) -> None:
     circuit_drawer(circuit, output_method="text")
-    # TODO: check if text is generated
+    out, _ = capfd.readouterr()
+    with open(expected_path, "r") as f:
+        expected = f.read()
 
-
-@pytest.mark.runlatex
-def test_output_method_latex() -> None:
-    circuit = QuantumCircuit(3)
-    circuit.add_X_gate(0)
-    circuit.add_X_gate(1)
-
-    circuit_drawer(circuit, output_method="latex")
-    # TODO: check if pdf file is generated
-
-
-def test_output_method_latex_source() -> None:
-    circuit = QuantumCircuit(3)
-    circuit.add_X_gate(0)
-    circuit.add_X_gate(1)
-    circuit_drawer(circuit, output_method="latex_source")
-    # TODO: check if latex file is generated
+    assert out == expected
