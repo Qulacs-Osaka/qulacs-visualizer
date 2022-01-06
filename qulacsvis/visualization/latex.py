6c712a39b169f97a3d4b4ac2960be0cc0eb3fac8
kawamochi <kotaro111024@gmail.com>
2022-01-06 20:50:47 +0900

    lint

--- qulacsvis/visualization/latex.py
+++ qulacsvis/visualization/latex.py
@@ -62,10 +62,12 @@ def _generate_latex_source(circuit: QuantumCircuit) -> str:
             name_latex_part = gate_dict[gate_data["raw_text"]]
             target_index_list = gate_data["target_bit"]
             control_index_list = gate_data["control_bit"]
-            if name_latex_part==r"\qswap":
+            if name_latex_part == r"\qswap":
                 for target_index in target_index_list:
                     gate_latex_part[target_index] = name_latex_part
-                gate_latex_part[target_index_list[0]] += r"\qwx[" + str(target_index_list[-1]-target_index_list[0]) + r"]"
+                gate_latex_part[target_index_list[0]] += (
+                    r"\qwx[" + str(target_index_list[-1] - target_index_list[0]) + r"]"
+                )
                 continue
 
             for target_index in target_index_list:
@@ -81,27 +83,31 @@ def _generate_latex_source(circuit: QuantumCircuit) -> str:
                     index = target_index_list[0]
                     size = 1
                     for t_index in target_index_list[1:]:
-                        if t_index==index+size:
-                            size+=1
+                        if t_index == index + size:
+                            size += 1
                         else:
-                            rle.append((index,size))
+                            rle.append((index, size))
                             index = t_index
                             size = 1
-                    rle.append((index,size))
+                    rle.append((index, size))
                     for rle_index in range(len(rle)):
-                        index,size = rle[rle_index]
-                        if size==1:
+                        index, size = rle[rle_index]
+                        if size == 1:
                             name_latex = r"\gate" + name_latex_part
                             gate_latex_part[index] = name_latex
                         else:
-                            name_latex = r"\multigate{" + str(size-1) + r"}" + name_latex_part
+                            name_latex = (
+                                r"\multigate{" + str(size - 1) + r"}" + name_latex_part
+                            )
                             gate_latex_part[index] = name_latex
-                            for k in range(1,size):
+                            for k in range(1, size):
                                 name_latex = r"\ghost" + name_latex_part
-                                gate_latex_part[index+k] = name_latex
-                        if rle_index+1<len(rle):
-                            next_index = rle[rle_index+1][0]
-                            gate_latex_part[index+size-1] += r"\qwx[" + str(next_index-index-size+1) +r"]"
+                                gate_latex_part[index + k] = name_latex
+                        if rle_index + 1 < len(rle):
+                            next_index = rle[rle_index + 1][0]
+                            gate_latex_part[index + size - 1] += (
+                                r"\qwx[" + str(next_index - index - size + 1) + r"]"
+                            )
 
             for control_index in control_index_list:
                 gate_latex_part[control_index] = (
