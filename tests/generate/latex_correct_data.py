17ff59f85bcdca302cdab998bdf7ecdc8572dafb
Hiroya-W <hyuyukun@gmail.com>
2022-01-11 17:42:26 +0900

    Add generator for latex drawer

---
+++ tests/generate/latex_correct_data.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+import matplotlib.pyplot as plt
+from qulacsvis import circuit_drawer
+
+sys.path.append(os.path.join("tests"))
+
+from circuit_test_data import load_circuit_data  # noqa
+
+OUTPUT_DIR = os.path.join("tests", "images", "latex")
+
+if __name__ == "__main__":
+    test_data = load_circuit_data()
+
+    for name, circuit in test_data.items():
+        img = circuit_drawer(circuit, "latex")
+        fig, ax = plt.subplots()
+        ax.imshow(img)
+        plt.savefig(os.path.join(OUTPUT_DIR, name + ".png"))
