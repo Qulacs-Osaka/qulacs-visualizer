0fd4cf1064142da26dafe550d4f0d156bc216008
Hiroya-W <hyuyukun@gmail.com>
2022-12-13 14:15:13 +0900

    :tada: Release 0.5.0

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Qulacs-Osaka"
 author = "Qulacs-Osaka"
 
 # The full version, including alpha/beta/rc tags
-release = "0.4.0"
+release = "0.5.0"
 
 
 # -- General configuration ---------------------------------------------------
